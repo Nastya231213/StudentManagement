e35b6f2316a0f5a327f4a21860997f778b8d9733
Nastya231213 <nastya1200526@gmail.com>
2024-01-17 21:18:48 +0200

    Addtion of the functional pagination to the classes page

--- private/controllers/Students.php
+++ private/controllers/Students.php
@@ -8,11 +8,10 @@ class Students extends Controller
         $user = new User();
         $school_id = Auth::getSchool_id();
 
-        $limit=2;
-        $page_number=isset($_GET['page'])?(int)$_GET['page']:1;
-        $page_number=$page_number<1?1:$page_number;
-        $offset=($page_number-1)*$limit;
-        $pager=new Pager();
+        $limit=1;
+     
+        $pager=new Pager($limit);
+        $offset=$pager->offset;
         $query = "select * from users where school_id =:school_id and rank in ('student') order by id desc limit $limit offset $offset";
         $arr[':school_id'] = $school_id;
         
@@ -26,7 +25,7 @@ class Students extends Controller
         $crumbs[] = ['Dashboard', ''];
         $crumbs[] = ['Staff', 'users'];
         if (Auth::access('reception')) {
-            $this->view("students", ['rows' => $data, 'crumbs' => $crumbs, 'mode' => 'students']);
+            $this->view("students", ['rows' => $data, 'crumbs' => $crumbs, 'mode' => 'students','pager'=>$pager]);
         } else {
             $this->view('access-denied');
         }
--- private/models/Pager.php
+++ private/models/Pager.php
@@ -1,45 +1,54 @@
-<?php 
+<?php
 
 
 
-class Pager {
+class Pager
+{
 
-    public $links=array();
-    public $offset=0;
-    public function __construct($limit=10){
-        $page_number=isset($_GET['page'])?(int)$_GET['page']:1;
-        $page_number=$page_number<1?1:$page_number;
-        $this->offset=($page_number-1)*$limit;
-        $current_link=ROOT."/".str_replace(["url=","index.php&"],"",$_SERVER['QUERY_STRING']);
-        $next_link=preg_replace('/page=[0-9]+/',"page=".($page_number+1),$current_link);
-        if($page_number==1){
-            $prev_link=preg_replace('/page=[0-9]+/',"page=1",$current_link);
+    public $links = array();
+    public $offset = 0;
+    public $page_number = 1;
+    public $start=1;
+    public $end=1;
 
-        }else{
-            $prev_link=preg_replace('/page=[0-9]+/',"page=".($page_number-1),$current_link);
-
+    public function __construct($limit = 10, $extras = 1)
+    {
+        $page_number = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $page_number = $page_number < 1 ? 1 : $page_number;
+        $this->offset = ($page_number - 1) * $limit;
+        $this->page_number=$page_number;
+        $this->end=$page_number+$extras;
+        $this->start=$page_number-$extras;
+        if($this->start<1){
+            $this->start=1; 
         }
-        $this->links['prev']=$prev_link;
 
-        $this->links['current']=$current_link;
-        $this->links['next']=$next_link;
+        $current_link = ROOT . "/" . str_replace(["url=", "index.php&"], "", $_SERVER['QUERY_STRING']);
+        $next_link = preg_replace('/page=[0-9]+/', "page=" . ($page_number + 1+$extras), $current_link);
+        $first_link = preg_replace('/page=[0-9]+/', "page=1", $current_link);
 
+        $this->links['first'] = $first_link;
+
+        $this->links['current'] = $current_link;
+        $this->links['next'] = $next_link;
     }
 
-    public function display(){
+    public function display()
+    {
 
-        ?>
-        <div >
-        <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
-        </nav>
-            </div>
-            <?php
+?>
+        <div>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item"><a class="page-link" href="<?=$this->links['first'];?>">First</a></li>
+                    <?php for($i=$this->start;$i<=$this->end;$i++):?>
+                    <li class="page-item <?=($i===$this->page_number)?'active':''; ?>"><a class="page-link" href="<?=preg_replace('/page=[0-9]+/', "page=" .$i,$this->links['current']);?>"><?=$i?></a></li>
+                   <?php endfor?>
+                    <li class="page-item"><a class="page-link" href="<?=$this->links['next']?>">Next</a></li>
+                </ul>
+            </nav>
+        </div>
+        <br>
+<?php
     }
 }
